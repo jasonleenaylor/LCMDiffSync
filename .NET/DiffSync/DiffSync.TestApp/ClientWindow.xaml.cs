d246a1af6772ce1e479bda2c253d18367a7a4c9e
Jason Naylor <jason_naylor@sil.org>
2023-03-23 08:32:11 -0700

    Add a WPF test app for debugging
    * Implement a server window and several client windows
    
    NOTE: This showed bugs in the implementation

---
+++ .NET/DiffSync/DiffSync.TestApp/ClientWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace DiffSync.TestApp
+{
+	/// <summary>
+	/// Interaction logic for ClientWindow.xaml
+	/// </summary>
+	public partial class ClientWindow : Window
+	{
+		private readonly ServerDocumentManager _serverDocumentManager = new ();
+		private IClientToServerCommunicator _changeCommunicator;
+		public ClientWindow(IClientToServerCommunicator changeCommunicator)
+		{
+			InitializeComponent();
+			_changeCommunicator = changeCommunicator;
+			_serverDocumentManager.OnContentChanged += HandleOnContentChange;
+		}
+
+		private void HandleOnContentChange()
+		{
+			textBox.TextChanged -= TextBox_TextChanged;
+			textBox.Text = _serverDocumentManager.Content["string"]?.ToString();
+			textBox.TextChanged += TextBox_TextChanged;
+			var tempDict = new Dictionary<Guid, DiffSyncDocument>();
+			tempDict[_serverDocumentManager.Guid] = _serverDocumentManager._serverDocument;
+			ServerShadowView.UpdateClientView(tempDict);
+		}
+		public ServerDocumentManager ServerDocumentManager => _serverDocumentManager;
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			_serverDocumentManager.Content["string"] = textBox.Text;
+			_serverDocumentManager.ApplyLocalChange();
+		}
+
+		private void Grid_Loaded(object sender, RoutedEventArgs e)
+		{
+			_serverDocumentManager.InitFromServer(_changeCommunicator, _serverDocumentManager.Guid);
+		}
+	}
+}
---
+++ .NET/DiffSync/DiffSync.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+
+namespace DiffSync.TestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ClientDocumentManager _documentManager;
+		private Guid _guid;
+		private DirectChangeCommunicator _changeCommunicator;
+
+		public MainWindow()
+		{
+			_guid = Guid.NewGuid();
+			_documentManager = new ClientDocumentManager();
+			_changeCommunicator = new DirectChangeCommunicator(_documentManager);
+			_documentManager.InitializeServer(JObject.Parse("{'string':''}"), _changeCommunicator, _guid);
+			_documentManager.OnContentChanged += ServerContentUpdate;
+			InitializeComponent();
+		}
+
+		private void ServerContentUpdate()
+		{
+			serverText.TextChanged -= TextBox_TextChanged;
+			if (serverText.Text != _documentManager.Content["string"]?.ToString())
+			{
+				serverText.Text = _documentManager.Content["string"]?.ToString();
+			}
+			serverText.TextChanged += TextBox_TextChanged;
+			ClientViews.UpdateClientView(_documentManager.clientDocuments);
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			SetText(_documentManager.Content, serverText.Text);
+		}
+
+		private void SetText(JObject content, string textboxContent)
+		{
+			content["string"] = textboxContent;
+		}
+
+		private void addClient_Click(object sender, RoutedEventArgs e)
+		{
+			var clientWindow = new ClientWindow(_changeCommunicator);
+			_changeCommunicator.AddClient(clientWindow.ServerDocumentManager.Guid, clientWindow.ServerDocumentManager);
+			clientWindow.Show();
+		}
+	}
+
+	public class DirectChangeCommunicator : IServerToClientCommunicator, IClientToServerCommunicator
+	{
+		private ClientDocumentManager _server;
+		private Dictionary<Guid, ServerDocumentManager> _clientManagers;
+		public Guid RemoteGuid { get; }
+
+		public DirectChangeCommunicator(ClientDocumentManager server)
+		{
+			_server = server;
+			_clientManagers = new Dictionary<Guid, ServerDocumentManager>();
+		}
+
+		public void SendClientEdits(Queue<IDocumentAction> edits)
+		{
+			_server.ApplyRemoteChangesToServer(edits);
+			
+		}
+
+		public void RequestDump(Guid clientGuid)
+		{
+			_server.SyncClient(clientGuid);
+		}
+
+		public void AddClient(Guid clientId, ServerDocumentManager clientManager)
+		{
+			_clientManagers[clientId] = clientManager;
+		}
+
+		public void SendServerEdits(Queue<IDocumentAction> edits)
+		{
+			foreach (var client in _clientManagers)
+			{
+				client.Value.ApplyRemoteChangesToClient(edits);
+			}
+		}
+	}
+}
