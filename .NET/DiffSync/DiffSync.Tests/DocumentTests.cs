aed2488e83cbc01f434f3ac5188f894edb7f297a
Kevin Hahn <hahn.kev@gmail.com>
2023-03-24 14:38:54 +0700

    swap out json patch for ycs.

---
+++ .NET/DiffSync/DiffSync.Tests/DocumentTests.cs
@@ -0,0 +1,12 @@
+﻿namespace DiffSync.Tests;
+
+public class DocumentTests
+{
+    [Test]
+    public void ClonesAreEqual()
+    {
+        var doc1 = new Document("{\"a\": 1}");
+        var doc2 = doc1.Clone();
+        Assert.That(doc1, Is.EqualTo(doc2));
+    }
+}
--- .NET/DiffSync/DiffSync/Document.cs
+++ .NET/DiffSync/DiffSync/Document.cs
@@ -1,41 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using JsonDiffPatchDotNet;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Ycs;
 
 namespace DiffSync
 {
     public class Document : IEquatable<Document>
     {
-        private static readonly JsonDiffPatch _differPatcher = new JsonDiffPatch(new Options { MinEfficientTextDiffLength = 2 });
-
+        private readonly YDoc _yDoc = new YDoc();
+        private readonly YMap _yMap;
         public Document(string json)
         {
-            _jObject = JObject.Parse(json);
+            _yMap = _yDoc.GetMap();
+            var jsonObject = JObject.Parse(json);
+            PopulateMap(_yMap, jsonObject);
         }
 
-        private readonly JObject _jObject;
+        private Document(Document? document)
+        {
+            if (document != null)
+            {
+                var state = document._yDoc.EncodeStateAsUpdateV2();
+                _yDoc.ApplyUpdateV2(state);
+            }
+
+            _yMap = _yDoc.GetMap();
+        }
+
+        private static void PopulateMap(YMap map, JObject jsonObject)
+        {
+            foreach (var (key, value) in jsonObject)
+            {
+                map.Set(key, JTokenToYValue(value));
+            }
+        }
+
+        private static object? JTokenToYValue(JToken? value)
+        {
+            switch (value)
+            {
+                case null:
+                    return null;
+                case JValue jValue:
+                    return jValue.Value;
+                case JObject jObject:
+                    var subMap = new YMap();
+                    PopulateMap(subMap, jObject);
+                    return subMap;
+                    break;
+                case JArray jArray:
+                    return new YArray(jArray.Select(JTokenToYValue));
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
 
         public void SetString(string field, string value)
         {
-            _jObject[field] = value;
+            _yMap.Set(field, value);
         }
 
         public string GetString(string field)
         {
-            return _jObject[field]?.ToString() ?? "";
+            return _yMap.Get(field)?.ToString() ?? "";
         }
 
         public Document Clone()
         {
-            return new Document(_jObject.ToString(Formatting.None));
+            return new Document(this);
         }
 
         public static Diff? Diff(Document? left, Document? right)
         {
-            var diff = _differPatcher.Diff(left?._jObject, right?._jObject);
-            if (diff == null) return null;
+            if (right is null) return null;
+            if (left == right) return null;
+            var diff = right._yDoc.EncodeStateAsUpdateV2(left?._yDoc.EncodeStateVectorV2());
             return new Diff(diff);
         }
 
@@ -47,15 +90,14 @@ namespace DiffSync
         public static Document? Patch(Document? left, Diff? diff)
         {
             if (diff == null) return left;
-
-            var patched = _differPatcher.Patch(left?._jObject, diff.JsonDiff);
-            return new Document(patched.ToString(Formatting.None));
+            var newDoc = new Document(left);
+            newDoc._yDoc.ApplyUpdateV2(diff.Update);
+            return newDoc;
         }
 
         public Document Patch(Diff diff)
         {
-            var patched = _differPatcher.Patch(_jObject, diff.JsonDiff);
-            return new Document(patched.ToString(Formatting.None));
+            return Patch(this, diff)!;
         }
 
 
@@ -63,7 +105,7 @@ namespace DiffSync
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return _jObject.ToString(Formatting.None).Equals(other._jObject.ToString(Formatting.None));
+            return _yDoc.CreateSnapshot().Equals(other._yDoc.CreateSnapshot());
         }
 
         public override bool Equals(object? obj)
@@ -76,7 +118,7 @@ namespace DiffSync
 
         public override int GetHashCode()
         {
-            return _jObject.GetHashCode();
+            return _yDoc.Guid.GetHashCode();
         }
 
         public static bool operator ==(Document? left, Document? right)
@@ -91,22 +133,22 @@ namespace DiffSync
 
         public override string ToString()
         {
-            return _jObject.ToString(Formatting.Indented);
+            return _yMap.ToDictionary(kvp => kvp.Key, kvp => kvp.Value).ToString();
         }
     }
 
     public class Diff
     {
-        public readonly JToken JsonDiff;
+        public byte[] Update { get; }
 
-        public Diff(JToken jsonDiff)
+        public Diff(byte[] update)
         {
-            JsonDiff = jsonDiff;
+            Update = update;
         }
 
         public Diff Clone()
         {
-            return new Diff(JsonDiff.DeepClone());
+            return new Diff(Update);
         }
     }
 }
