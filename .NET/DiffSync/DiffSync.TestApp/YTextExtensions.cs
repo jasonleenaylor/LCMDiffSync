93ffa20973166eec7b35411e716f9bca5bff9433
Kevin Hahn <hahn.kev@gmail.com>
2023-03-31 09:51:45 +0700

    add an error for not supported case

--- .NET/DiffSync/DiffSync.TestApp/YTextExtensions.cs
+++ .NET/DiffSync/DiffSync.TestApp/YTextExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using Ycs;
 
 namespace DiffSync.TestApp;
@@ -11,6 +12,8 @@ public static class YTextExtensions
         {
             //todo according to the documentation both AddedLength and RemovedLength can be greater than 1, I'm not really sure
             //how to solve that, but for our simple app this doesn't come up
+            if (change.AddedLength > 0 && change.RemovedLength > 0)
+                throw new NotSupportedException("characters both added and removed in the same event, not currently supported");
             if (change.AddedLength > 0)
             {
                 text.Insert(change.Offset, newString.Substring(change.Offset, change.AddedLength));
